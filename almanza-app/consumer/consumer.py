18649d896583eabd7cb53592f1abfb2e9b4f58bb
Alvaro Almanza <alvaroalmanza@MacBook-Pro-de-Alvaro.local>
2020-10-07 22:05:58 +0200

    fix consumer

--- almanza-app/consumer/consumer.py
+++ almanza-app/consumer/consumer.py
@@ -11,7 +11,7 @@ def main():
     def callback(ch, method, properties, body):
         print(" [x] Received %r" % body.decode())
 
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue='alvaroalmanza', on_message_callback=callback, auto_ack=True)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
